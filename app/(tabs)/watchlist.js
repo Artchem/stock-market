20e8c6eea28c77ada50a36c440d92e38e6695f25
Artchem <chemerys.artem@gmail.com>
2024-04-01 19:12:57 +0200

    add watchlist

--- app/(tabs)/watchlist.js
+++ app/(tabs)/watchlist.js
@@ -1,7 +1,74 @@
-import { StyleSheet, View } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, View, FlatList, Pressable, Animated } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
+import { Text } from "react-native-paper";
+import StockCard from "../../components/StockCard";
+import { stocks } from "../../data";
+import { StoreContext } from "../_layout";
+
+function RightActions({ process, dragX, onPress }) {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [0.7, 0],
+  });
+
+  return (
+    <Pressable
+      style={{
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 90,
+      }}
+      onPress={onPress}
+    >
+      <Animated.Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 22,
+          color: "white",
+          transform: [{ scale }],
+        }}
+      >
+        Delete
+      </Animated.Text>
+    </Pressable>
+  );
+}
 
 export default function WatchListScreen() {
-  return <View></View>;
+  const { likedStocks, updadeLikedStocks } = useContext(StoreContext);
+
+  if (likedStocks.length > 0)
+    return (
+      <View style={{ flex: 1 }}>
+        <FlatList
+          keyExtractor={(item) => item.ticker}
+          data={stocks.filter((i) => likedStocks.includes(i.ticker))}
+          renderItem={({ item }) => (
+            <Swipeable
+              renderRightActions={(process, dragX) => (
+                <RightActions
+                  progress={process}
+                  dragX={dragX}
+                  onPress={() => updadeLikedStocks(item.ticker, "del")}
+                />
+              )}
+            >
+              <StockCard {...item} />
+            </Swipeable>
+          )}
+        />
+      </View>
+    );
+
+  return (
+    <View style={styles.container}>
+      <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
+        No Stocks On Watchlist
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
